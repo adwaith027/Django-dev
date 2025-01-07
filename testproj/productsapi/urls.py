7d0b929fa372c49a5be28ad8225a5d460ee5c4e9
adwaith027 <adwaithh227@gmail.com>
2025-01-07 12:19:37 +0530

    api crud

--- testproj/productsapi/urls.py
+++ testproj/productsapi/urls.py
@@ -6,6 +6,6 @@ urlpatterns=[
     path('simpleapi/',views.simpleapi,name='simpleapi'),
     path('signup/',views.signup,name='signup_api'),
     path('login/', views.login, name='login_api'),
-    path('createproduct/',views.createProduct,name='createProductapi'),
-    path('listproduct/',views.listProduct,name='listProductapi'),
+    path('createproduct/',views.createProduct,name='createProduct_api'),
+    path('listproduct/',views.listProduct,name='listProduct_api'),
 ]
